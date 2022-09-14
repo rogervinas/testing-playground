705d792ad2548ccb37c289261bbcb5c5b351eae8
Roger Viñas <roger.vinas@n26.com>
2022-09-14 10:02:28 +0200

    Use kotlin when

--- mutation-testing/src/main/kotlin/org/testingplayground/MyImpl.kt
+++ mutation-testing/src/main/kotlin/org/testingplayground/MyImpl.kt
@@ -1,18 +1,11 @@
 package org.testingplayground
 
 class MyImpl {
-    fun doSomething(a: Int, b: Int): String {
-        if (a < 0 || b < 0) {
-            return "Either A or B are negative"
-        }
-        if (a == 0 && b == 0) {
-            return "Both A and B are zero"
-        }
-        if (a > b) {
-            return "A is greater than B"
-        }
-        return if (b > a) {
-            "B is greater than A"
-        } else "A and B are equal"
+    fun doSomething(a: Int, b: Int) = when {
+        (a < 0 || b < 0) -> "Either A or B are negative"
+        (a == 0 && b == 0) -> "Both A and B are zero"
+        (a > b) -> "A is greater than B"
+        (b > a) -> "B is greater than A"
+        else -> "A and B are equal"
     }
 }
